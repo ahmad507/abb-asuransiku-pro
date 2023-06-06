1e9d6d4f4f0ee119fb397ee9b5aa5eff05ec2839
ahmad ade rozak <ahmadrozak@versatech.co.id>
2023-06-06 07:10:47 +0700

    ST-001
    Signed-off-by: ahmad ade rozak <ahmadrozak@versatech.co.id>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,48 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {IntroGuard} from "./guards/intro/intro.guard";
+import {AutologinGuard} from "./guards/autologin/autologin.guard";
+import {AuthenticationGuard} from "./guards/auth/authentication.guard";
 
 const routes: Routes = [
-  {
+  //---------------------------------------------------------------------------------------------------
+  /*
+  * Tidak Dipakai
+  * */
+  /*{
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'message/:id',
     loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
+  },*/
+  //---------------------------------------------------------------------------------------------------
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canLoad: [IntroGuard, AutologinGuard]
+  },
+  {
+    path: 'intro',
+    loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule),
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
+    canLoad: [AuthenticationGuard]
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule),
+    canLoad: [AuthenticationGuard]
+  },
+
 ];
 
 @NgModule({
